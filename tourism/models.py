f29b067b46eefca54bdae7d48a4757f10cf873a3
Victor Waburi <victorwaburi@gmail.com>
2022-06-10 22:28:03 +0300

    Added media folder, created database

--- tourism/models.py
+++ tourism/models.py
@@ -7,5 +7,5 @@ class Place(models.Model):
     name = models.CharField(max_length=100)
     img = models.ImageField(upload_to='photos')
     date = models.DateField()
-    desc  = models.TextField
+    desc  = models.TextField(default='Add something')
     posts = models.BooleanField(default=False)
